da95a377ca6a7114a500da5f2aa3ec56cd500983
Malik Amir <142987219+Malik-Amir30@users.noreply.github.com>
2023-10-23 17:19:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "malik@goolge.com"
+var accountPassword = "12345"
+accountCity = "Rawalpindi"
+let accountState;
+accountEmail = "hc@hc.com"
+accountPassword = "123234"
+accountCity = "Islamabad"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+"use strict";
