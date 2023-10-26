a0c2ee4d3f1cbabba971b52d7adcca1265cd649c
Malik Amir <142987219+Malik-Amir30@users.noreply.github.com>
2023-10-26 18:28:21 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = 33;
+let test = "33 Testing";
+
+
+console.log(typeof score);
+console.log(typeof(test));
+
+let valueInNumber = Number(test);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+ 
+// 33 => 33
+// true => 1 or false => 0
+// 33 tesing => NAN
